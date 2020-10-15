a51796e86d569952d326e4e159b2683c0ac9dbd8
Your Name <you@example.com>
2020-10-15 11:27:03 +1100

    Displaying using for loop

--- sketch.js
+++ sketch.js
@@ -1,35 +1,67 @@
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
-var particles=[]
-var plinko=[]
-var division=[]
+var particles = []
+var plinkos = []
+var divisions = []
 
-var divisionHeight=300
+var divisionHeight = 300
 
 function setup() {
-  createCanvas(480,750);
-  engine = Engine.create();
-  world = engine.world;
+    createCanvas(800, 800);
+    engine = Engine.create();
+    world = engine.world;
+    ground = new Ground(width / 2, height, width, 20);
 
 
- 
-  ground=new Ground(width/2,740,width,20)
- 
+    for (var k = 0; k <= width; k = k + 80) {
+        divisions.push(new Division(k, height - divisionHeight / 2, 10, divisionHeight));
+    }
+
+
+    for (var j = 75; j <= width; j = j + 50) {
+
+        plinkos.push(new Plinko(j, 75));
+    }
+
+    for (var j = 50; j <= width - 10; j = j + 50) {
+
+        plinkos.push(new Plinko(j, 175));
+    }
+
+    for (var j = 75; j <= width; j = j + 50) {
+
+        plinkos.push(new Plinko(j, 275));
+    }
+
+    for (var j = 50; j <= width - 10; j = j + 50) {
+
+        plinkos.push(new Plinko(j, 375));
+    }
 
 }
 
 function draw() {
-  background(0);  
-  Engine.update(engine);
+    background("black");
+    textSize(20)
+    Engine.update(engine);
+    ground.display();
 
-  ground.display()
-  for(var i=0;i<=width;i=i+80){
-    division.push(new Division(i,height-divisionHeight/2,10,divisionHeight))
-    
-  }
+    for (var i = 0; i < plinkos.length; i++) {
+        plinkos[i].display();
+    }
 
- drawSprites()
+    if (frameCount % 60 === 0) {
+        particles.push(new Particle(random(width / 2 - 30, width / 2 + 30), 10, 10));
+    }
+
+    for (var j = 0; j < particles.length; j++) {
+        particles[j].display();
+    }
+
+    for (var k = 0; k < divisions.length; k++) {
+        divisions[k].display();
+    }
 }
